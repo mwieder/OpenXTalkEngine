5bd66b8ee62c6fab0403fd931b4e7b44ed6eac2a
Mark Wieder <mwieder@ahsoftware.net>
2020-11-08 16:38:10 -0800

    builds on linux, prebuilds build for armv7

--- config-rpi.sh
+++ config-rpi.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 
-./config.sh --cross --platform linux-armv6hf --cc-prefix ${HOME}/x-tools/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf- --sysroot ${HOME}/x-tools/arm-rpi-linux-gnueabihf/arm-rpi-linux-gnueabihf/sysroot --aux-sysroot /var/lib/schroot/chroots/jessie-armhf --triple arm-linux-gnueabihf
+./config.sh --cross --platform linux-armv7 --cc-prefix ${HOME}/x-tools/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf- --sysroot ${HOME}/x-tools/arm-rpi-linux-gnueabihf/arm-rpi-linux-gnueabihf/sysroot --aux-sysroot /var/lib/schroot/chroots/jessie-armhf --triple arm-linux-gnueabihf
+
+#./config.sh --cross --platform linux-armv6hf --cc-prefix ${HOME}/x-tools/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf- --sysroot ${HOME}/x-tools/arm-rpi-linux-gnueabihf/arm-rpi-linux-gnueabihf/sysroot --aux-sysroot /var/lib/schroot/chroots/jessie-armhf --triple arm-linux-gnueabihf
