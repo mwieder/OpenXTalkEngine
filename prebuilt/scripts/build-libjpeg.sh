379a1a1893bb7f5ddc3ca88d4e4a21f5af9ce5c0
Mark Wieder <mwieder@ahsoftware.net>
2024-11-23 10:17:08 -0800

    libjpeg working with version 9b

--- prebuilt/scripts/build-libjpeg.sh
+++ prebuilt/scripts/build-libjpeg.sh
@@ -4,27 +4,33 @@ source "${BASEDIR}/scripts/platform.inc"
 source "${BASEDIR}/scripts/lib_versions.inc"
 source "${BASEDIR}/scripts/util.inc"
 
-# 2024.07.22 currently ${libjpeg_VERSION} is 6b
+# 2024.07.22 currently ${libjpeg_VERSION} is 9b
 #https://sourceforge.net/projects/libjpeg/files/latest/download
+#https://ijg.org/files/jpegsrc.v9b.tar.gz
 
 SHORT_THIS="jpeg"
 THIS="libjpeg"
-URL_ROOT="https://sourceforge.net/projects/${THIS}/files/latest/download"
+#URL_ROOT="https://sourceforge.net/projects/${THIS}/files/latest/download"
+#URL_ROOT="https://ijg.org/files/${SHORT_THIS}sr${libjpeg_VERSION}.zip"
+URL_ROOT="https://ijg.org/files/${SHORT_THIS}src.v${libjpeg_VERSION}.tar.gz"
 # should do this with sed
-ARCHIVE_DESTINATION="${SHORT_THIS}sr6"
+JPEG_MAJOR_VERSION=$(echo "${libjpeg_VERSION}" | sed 's/[a-z]//')
+ARCHIVE_DESTINATION="${SHORT_THIS}sr${JPEG_MAJOR_VERSION}"
 FILE_DIRECTORY="../../thirdparty/${THIS}/src"
 
 fetchBinary
 rm -rf ${SHORT_THIS}-${libjpeg_VERSION}
-unxzBinary
+#unxzBinary
+untarBinary
 mv ${SHORT_THIS}-${libjpeg_VERSION} ${ARCHIVE_DESTINATION}
 # have to convert the windows-style line endings to linux
+# sudo apt install dos2unix
 pushd ${BUILDDIR}/${ARCHIVE_DESTINATION}
 if [ -e "${BUILDDIR}/${ARCHIVE_DESTINATION}/configure" ] ; then
+	dos2unix configure
 	echo "running configure"
 	./configure
 fi
 makeBinary
 popd
 buildLibrary
-
