b0ef775966963901a129e428b78a338ebf92c03c
Mark Wieder <mwieder@ahsoftware.net>
2025-03-14 08:42:33 -0700

    updated libpng script

--- prebuilt/scripts/build-libpng.sh
+++ prebuilt/scripts/build-libpng.sh
@@ -4,27 +4,31 @@ source "${BASEDIR}/scripts/platform.inc"
 source "${BASEDIR}/scripts/lib_versions.inc"
 source "${BASEDIR}/scripts/util.inc"
 
-# 2024.07.22 currently ${LIBZ_VERSION} is 1.6.44
-#libpng-1.6.44.tar.xz
+# 2025.01.28 ignore the specified version
+# and grab the version number from the downloaded file
+#libpng-1.6.46.tar.xz
 
 THIS="libpng"
-# should do this with sed
-PNG_VERSION=$(echo ${libpng_VERSION} | sed 's/\.//g')
-#UNTARRED="lpng1644"
-UNTARRED="lpng${PNG_VERSION}"
+# remove the dots with sed
+#PNG_VERSION=$(echo ${libpng_VERSION} | sed 's/\.//g')
 URL_ROOT="https://sourceforge.net/projects/${THIS}/files/latest/download"
-ARCHIVE_DESTINATION="${THIS}-${libpng_VERSION}"
+ARCHIVE_DESTINATION=${THIS}
 FILE_DIRECTORY="../../thirdparty/${THIS}/src"
 
 function unxzBinary
 {
-	echo "Untarring ${THIS}-${libpng_VERSION}"
-	unzip -o "${THIS}-${libpng_VERSION}.tar"
+	echo "Untarring ${THIS}.tar"
+	unzip -o "${THIS}.tar" -d "lpng"
+	# retrieve the latest version number (1646)
+	PNG_LATEST=$(echo `ls lpng` | sed 's/[^0-9]//g')
+	ARCHIVE_DESTINATION="${THIS}${PNG_LATEST}/lpng${PNG_LATEST}"
+	mv "libpng.tar" "libpng${PNG_LATEST}.tar"
+	echo "ARCHIVE_DESTINATION= ${ARCHIVE_DESTINATION}"
+	echo "Renaming lpng to ${THIS}${PNG_LATEST}"
+	mv lpng "${THIS}${PNG_LATEST}"
+	# now we should have a directory named libpng1646
 }
-
 
 fetchBinary
 unxzBinary
-mv "${UNTARRED}" "${ARCHIVE_DESTINATION}"
 buildLibrary
-
